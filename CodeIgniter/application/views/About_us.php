75398131a5d267a2ef86aa3acf0308f6ede30aca
Aloka Perera <alokatperera@gmail.com>
2025-09-26 10:40:31 +0530

    💕

--- CodeIgniter/application/views/About_us.php
+++ CodeIgniter/application/views/About_us.php
@@ -5,5 +5,7 @@
 <body>
     <h1>Welcome to ICT413 class</h1>
     <h3><?php echo $heading ?></h3>
-    <p><?php echo $message ?></p>
+   <h6><?php echo $message ?></h6>
+   <h2>About Us</h2>
+   <p>This is the About Us section of the website.</p>
 </body>
